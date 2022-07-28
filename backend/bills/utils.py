e0e93ea9cb0c1a3e2e5310adc3fc9dce9c29489a
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-28 10:50:53 +0300

    Fix error in xlsx parsing

--- backend/bills/utils.py
+++ backend/bills/utils.py
@@ -11,7 +11,7 @@ def get_bills_list(xlsx=f'{DATA_DIR}/bills.xlsx'):
     for column in sheet.iter_cols(1, sheet.max_column):
         col_names.append(column[0].value)
     bills = []
-    for row in range(2, sheet.max_row):
+    for row in range(2, sheet.max_row+1):
         bill = {}
         for column in range(1, sheet.max_column):
             bill[col_names[column-1]] = sheet.cell(row=row,
