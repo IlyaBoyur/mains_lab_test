be51cc43bb154c0fbcec77fbdb2efedcab32171d
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-28 10:41:27 +0300

    Add Bill model

--- backend/bills/models.py
+++ backend/bills/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Bill(models.Model):
+    client_name = models.CharField(
+        'Имя клиента',
+        max_length=200,
+    )
+    client_org = models.CharField(
+        'Организация клиента',
+        max_length=200,
+    )
+    account_number = models.PositiveIntegerField(
+        'Номер-счёт',
+    )
+    sum = models.DecimalField(
+        'Сумма',
+        decimal_places=2,
+        max_digits=20,
+    )
+    date = models.DateTimeField(
+        'Дата',
+    )
+    service = models.CharField(
+        'Название сервиса',
+        max_length=200,
+    )
+
+    class Meta:
+        verbose_name = 'Счёт'
+        verbose_name_plural = 'Счета'
+
+    def __str__(self):
+        return f'{self.date} | {self.account_number} | {self.sum}'
