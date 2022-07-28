6f4503b4b99b74e3a2d05d30499649c32472e994
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-28 10:45:10 +0300

    Add bill admin

--- backend/bills/admin.py
+++ backend/bills/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bill
+
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = ('client_name',
+                    'client_org',
+                    'account_number',
+                    'sum',
+                    'date',
+                    'service')
