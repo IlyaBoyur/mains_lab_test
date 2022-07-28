0615a19f68931bb6642950fc5bad73720d114dc2
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-28 10:45:46 +0300

    Add util for biil import

---
+++ backend/bills/utils.py
@@ -0,0 +1,20 @@
+
+import openpyxl
+
+from config.settings import DATA_DIR
+
+
+def get_bills_list(xlsx=f'{DATA_DIR}/bills.xlsx'):
+    wb = openpyxl.load_workbook(xlsx)
+    sheet = wb.active
+    col_names = []
+    for column in sheet.iter_cols(1, sheet.max_column):
+        col_names.append(column[0].value)
+    bills = []
+    for row in range(2, sheet.max_row):
+        bill = {}
+        for column in range(1, sheet.max_column):
+            bill[col_names[column-1]] = sheet.cell(row=row,
+                                                   column=column).value
+        bills.append(bill)
+    return bills
