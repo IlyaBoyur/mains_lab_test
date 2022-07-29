6f918f3681c28c410d9516422ec78afba711b811
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-29 12:25:21 +0300

    Update utils

--- backend/bills/utils.py
+++ backend/bills/utils.py
@@ -1,20 +1,33 @@
+from datetime import datetime
+from typing import Any, Dict, List
 
 import openpyxl
-
 from config.settings import DATA_DIR
 
 
-def get_bills_list(xlsx=f'{DATA_DIR}/bills.xlsx'):
-    wb = openpyxl.load_workbook(xlsx)
+def parse_excel(xlsx_file: str) -> List[Dict[str, Any]]:
+    """Retrieve list of items constructed from excel table row."""
+    wb = openpyxl.load_workbook(xlsx_file)
     sheet = wb.active
     col_names = []
     for column in sheet.iter_cols(1, sheet.max_column):
         col_names.append(column[0].value)
-    bills = []
+    items = []
     for row in range(2, sheet.max_row+1):
-        bill = {}
+        item = {}
         for column in range(1, sheet.max_column):
-            bill[col_names[column-1]] = sheet.cell(row=row,
+            item[col_names[column-1]] = sheet.cell(row=row,
                                                    column=column).value
-        bills.append(bill)
-    return bills
+        items.append(item)
+    return items
+
+
+def get_bills_list(xlsx=f'{DATA_DIR}/bills.xlsx'):
+    """Retrieve and filter bills"""
+    bills = parse_excel(f'{DATA_DIR}/bills.xlsx')
+    return [bill for bill in bills
+            if (
+                bill['service'] != '-'
+                and isinstance(bill['date'], datetime)
+                and isinstance(bill['№'], int)
+            )]
